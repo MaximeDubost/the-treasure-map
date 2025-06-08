821bc18782549f7fa3ef638dc72341be18b872cd
Maxime <maxime.dubost.75@gmail.com>
2025-06-08 10:34:57 +0200

    build(): initialize spring boot app

---
+++ src/main/java/fr/mdbs/thetreasuremap/TheTreasureMapApplication.java
@@ -0,0 +1,13 @@
+package fr.mdbs.thetreasuremap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheTreasureMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheTreasureMapApplication.class, args);
+    }
+
+}
